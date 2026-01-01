098d741c12f60f10a4eab3624a7e26c5698a7cff
Tjay05 <tjayolatayo@gmail.com>
2026-01-01 18:22:37 +0100

    Add artisan route setup

---
+++ server.js
@@ -0,0 +1,19 @@
+require('dotenv').config();
+const express = require('express');
+const cors = require('cors');
+const connectDB = require('./config/db');
+const artisanRoutes = require('./routes/artisan');
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+app.use((req, res, next) => {
+  console.log(req.path, req.method);
+  next();
+});
+
+app.use('/api/artisan', artisanRoutes);
+
+connectDB(app);
