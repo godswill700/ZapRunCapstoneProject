5985dd2a47a1b540ffab3b9ddf4d358b0cf1f27e
Tjay05 <tjayolatayo@gmail.com>
2026-01-04 18:05:32 +0100

    Create verify artisan email function

--- controllers/artisan.controller.js
+++ controllers/artisan.controller.js
@@ -2,31 +2,32 @@ const bcrypt = require('bcrypt');
 const jwt = require('jsonwebtoken');
 const validator = require('validator')
 const Artisan = require('../models/artisanModel');
+const Otp = require('../models/otpModel');
 const sendOtp = require('../utils/sendOtp');
 
 // Create token
 const createToken = (_id) => {
-  return jwt.sign({_id}, process.env.SECRET, { expiresIn: '7d' })
+  return jwt.sign({_id}, process.env.SECRET, { expiresIn: '2d' })
 }
 
 // Register Artisan
 const registerArtisan = async (req, res) => {
   const { fullName, phone, email, password, serviceType, location } = req.body;
 
-  if (!fullName || !phone || !email || !password || !serviceType || !location) {
-    return res.status(400).json({message: 'All fields must be filled'});
-  }
-  if (!validator.isEmail(email)) {
-    return res.status(400).json({message: 'Email is not valid'});
-  }
-  if (!validator.isStrongPassword(password)) {
-    return res.status(400).json({message: 'Password must include special characters, lowercase, uppercase and be minimum 8 characters'});
-  }
-
-  const exists = await Artisan.findOne({ email });
-  if (exists) res.status(400).json({message: 'Email already in use'});
-
   try {
+    if (!fullName || !phone || !email || !password || !serviceType || !location) {
+      return res.status(400).json({message: 'All fields must be filled'});
+    }
+    if (!validator.isEmail(email)) {
+      return res.status(400).json({message: 'Email is not valid'});
+    }
+    if (!validator.isStrongPassword(password)) {
+      return res.status(400).json({message: 'Password must include special characters, lowercase, uppercase and be minimum 8 characters'});
+    }
+
+    const exists = await Artisan.findOne({ email });
+    if (exists) res.status(400).json({message: 'Email already in use'});
+  
     const artisan = await Artisan.create({fullName, phone, email, password, serviceType, location});
     
     sendOtp(artisan.email, res);
@@ -35,22 +36,53 @@ const registerArtisan = async (req, res) => {
   }
 };
 
+// Verify Artisan's Email
+const verifyArtisanEmail = async (req, res) => {
+  const { email, otp } = req.body;
+  try {
+    if (!email || !otp) return res.status(400).json({message: "Please fill in OTP fields!!!"});
+  
+    const otpRecord = await Otp.find({ email });
+    if (otpRecord.length <= 0) return res.status(400).json({message: "Artisan record does not exist or has been verified already. Please sign up or log in."});
+  
+    const hashedOtp = otpRecord[0].otp;
+  
+    const validOtp = await bcrypt.compare(otp, hashedOtp);
+    if (!validOtp) return res.status(400).json({message: "Invalid otp, confirm the code in your inbox"});
+  
+    // Mark enail as verified
+    await Artisan.updateOne({ email }, { isEmailVerified: true });
+  
+    // Delete otp Record
+    await Otp.deleteMany({ email });
+
+    const artisan = await Artisan.findOne({ email });
+
+    // create token
+    const token = createToken(artisan._id);
+    res.status(200).json({email, token})
+  } catch (error) {
+    res.status(400).json({error: error.message})
+  }
+}
+
+// Login Artisan
 const loginArtisan = async (req, res) => {
   const { email, password } = req.body;
 
-  if (!email || !password) return res.status(400).json({message: "Please fill in all fields!!!"});
-
-  const artisan = Artisan.findOne({ email });
-
-  if (!artisan) return res.status(400).json({message: "Incorrect email"});
-
-  const match = await bcrypt.compare(password, user.password);
-
-  if (!match) return res.status(400).json({message: "Incorrect password"});
-
   try {
+    if (!email || !password) return res.status(400).json({message: "Please fill in all fields!!!"});
+
+    const artisan = await Artisan.findOne({ email });
+
+    if (!artisan) return res.status(400).json({message: "Incorrect email"});
+
+    const match = await bcrypt.compare(password, artisan.password);
+
+    if (!match) return res.status(400).json({message: "Incorrect password"});
+
     // create token
-    const token = createToken(user._id);
+    const token = createToken(artisan._id);
     res.status(200).json({email, token})
   } catch (error) { 
     res.status(400).json({error: error.message})
@@ -59,5 +91,6 @@ const loginArtisan = async (req, res) => {
 
 module.exports = {
   registerArtisan,
-  loginArtisan
+  loginArtisan,
+  verifyArtisanEmail
 };
