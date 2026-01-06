77357315a45b7fb099e0ca4972397263729927cf
Tjay05 <tjayolatayo@gmail.com>
2026-01-06 13:35:44 +0100

    Add admin route

---
+++ routes/admin.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const { registerAdmin, resendOtp, verifyEmail, loginAdmin } = require('../controllers/admin.controller');
+
+const router = express.Router();
+
+// signup route
+router.post('/signup', registerAdmin);
+
+// resend otp for route
+router.post('/resend-otp', resendOtp);
+
+// login route
+router.post('/login', loginAdmin);
+
+// Verify email route
+router.post('/verify-email', verifyEmail);
