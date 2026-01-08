2ccaf2e81bef0257c40cb70d7c5c18799626ce3a
Tjay05 <tjayolatayo@gmail.com>
2026-01-08 13:39:55 +0100

    Update job model

---
+++ models/Job.js
@@ -0,0 +1,23 @@
+const mongoose = require("mongoose");
+
+const jobSchema = new mongoose.Schema(
+  {
+    title: { type: String, required: true },
+    service: { 
+      type: String, 
+      enum: ["plumber", "painter", "electrician"], 
+      required: true 
+    },
+    description: { type: String, required: true },
+    assignedArtisan: { type: mongoose.Schema.Types.ObjectId, ref: "Artisan", default: null },
+    status: {
+      type: String,
+      enum: ["pending", "accepted", "in-progress", "completed", "cancelled"],
+      default: "pending",
+    },
+    createdBy: { type: mongoose.Schema.Types.ObjectId, ref: "User", required: true },
+  },
+  { timestamps: true }
+);
+
+module.exports = mongoose.model("Job", jobSchema);
