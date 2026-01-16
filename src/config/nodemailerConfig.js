3c82fdf16b0c329bb06b47cdcc9b60a6c922aaf8
Tjay05 <tjayolatayo@gmail.com>
2026-01-16 01:21:43 +0100

    Reconfigure nodemailer with app password

--- src/config/nodemailerConfig.js
+++ src/config/nodemailerConfig.js
@@ -1,33 +1,16 @@
-const { google } = require('googleapis');
+// const { google } = require('googleapis');
 const nodemailer = require('nodemailer');
 
 const AUTH_EMAIL = process.env.AUTH_EMAIL;
-const CLIENT_ID = process.env.CLIENT_ID;
-const CLIENT_SECRET = process.env.CLIENT_SECRET;
-const REFRESH_TOKEN = process.env.REFRESH_TOKEN;
-
-const REDIRECT_URI = "https://developers.google.com/oauthplayground";
-
-const oAuth2Client = new google.auth.OAuth2(
-  CLIENT_ID,
-  CLIENT_SECRET,
-  REDIRECT_URI
-);
-
-oAuth2Client.setCredentials({ refresh_token: REFRESH_TOKEN });
+const EMAIL_PASS = process.env.EMAIL_PASS;
 
 const createTransporter = async () => {
   try {
-    const accessToken = await oAuth2Client.getAccessToken();
-
     const transporter = nodemailer.createTransport({
       service: "gmail",
       auth: {
-        type: "OAuth2",
         user: AUTH_EMAIL,
-        clientId: CLIENT_ID,
-        clientSecret: CLIENT_SECRET,
-        refreshToken: REFRESH_TOKEN,
+        pass: EMAIL_PASS
       },
     });
 
